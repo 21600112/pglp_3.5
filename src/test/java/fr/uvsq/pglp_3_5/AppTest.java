22e2c4c357a1934f3b529aefb6139c60021a4f9d
Darksice <Darksice99@gmail.com>
2020-05-16 15:46:49 +0200

    test

--- src/test/java/fr/uvsq/pglp_3_5/AppTest.java
+++ src/test/java/fr/uvsq/pglp_3_5/AppTest.java
@@ -1,38 +1,22 @@
 package fr.uvsq.pglp_3_5;
 
-import junit.framework.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void robotAvanceNord()
+	{	
+		AfficheMetier a = new AfficheMetier();
+		UneClasseMetier ucm = new UneClasseMetier(a);
+		ucm.afficheInfo();
+	}
+	
+	
 }
